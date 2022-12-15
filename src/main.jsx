bd3bae84f394323d624355ea12df9b796c5692e4
budhamud <nigitart@gmail.com>
2022-12-15 11:54:13 -0300

    hotfix en main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from 'styled-components'
 import App from './App'
-import { theme } from './Context/Theme'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
